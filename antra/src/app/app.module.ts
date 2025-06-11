1999876baadce3fafc1539f9bc92b89549454a34
bilguungzt <54411261+bilguungzt@users.noreply.github.com>
2025-06-11 07:33:59 -0700

    Reactive form

---
+++ antra/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserprofileComponent } from './userprofile/userprofile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, UserprofileComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ antra/src/app/userprofile/userprofile.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-userprofile',
+  templateUrl: './userprofile.component.html',
+  styleUrl: './userprofile.component.scss',
+})
+export class UserprofileComponent {
+  counter = 0;
+
+  userProfileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    dob: new FormControl(''),
+  });
+
+  get Name(): string | null | undefined {
+    return this.userProfileForm.value.name;
+  }
+
+  get Email(): string | null | undefined {
+    return this.userProfileForm.value.email;
+  }
+
+  get DoB(): string | null | undefined {
+    return this.userProfileForm.value.dob;
+  }
+
+  get welcome(): string {
+    if (this.Name) return `Welcome, ${this.Name}!`;
+    return '';
+  }
+
+  get isInputEmpty(): boolean {
+    return Object.values(this.userProfileForm.controls).every(
+      (control) => !control.value
+    );
+  }
+
+  onReset() {
+    this.userProfileForm.reset();
+  }
+
+  onSave() {
+    this.counter++;
+    Object.values(this.userProfileForm.value).map((v) => console.log(v));
+  }
+}
